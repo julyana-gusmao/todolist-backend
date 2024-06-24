e30a91bd907023cf008b5491d76831ffba0ee3c6
Julyana Gusmao <the2kira@hotmail.com>
2024-06-24 20:01:07 -0300

    db funcionando

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const todoRoutes = require("./todo.routes");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(todoRoutes);
 
 app.get("/health", (req, res) => {
